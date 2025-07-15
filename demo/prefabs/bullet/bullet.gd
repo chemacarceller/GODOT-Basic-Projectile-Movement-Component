a5ce327b1cda9b0a85520267754e6acb8f4391a8
Ubuntu <chemacarceller@yahoo.es>
2025-07-15 10:15:43 +0200

    last changes to demo

--- demo/prefabs/bullet/bullet.gd
+++ demo/prefabs/bullet/bullet.gd
@@ -1,5 +1,7 @@
 extends StaticBody3D
 
+# This method is called when the basic-projectile-movement-component detects a collision
+# Configure as a parameter in the component
 func _on_basic_projectile_movement_stb__collision_taken_place(_collision : KinematicCollision3D) -> void:
 	# When hitting anything it is eliminated. no further action
 	queue_free()
--- demo/prefabs/weapon/assault_rifle_1.gd
+++ demo/prefabs/weapon/assault_rifle_1.gd
@@ -6,15 +6,18 @@ extends Node3D
 var _isFireEnebled : bool = true
 
 func _input(_event) -> void:
-	# If the character is moving
-	if owner.get_movementComponent().get_isMoving():
+	# If the character is moving and not jumping nor falling is allowed to shoot
+	if owner.get_movementComponent().get_isMoving() and not owner.get_movementComponent().get_isJumping() and not owner.get_movementComponent().get_isFalling():
 		
 		# Firing
 		if Input.is_action_pressed("fire") and _isFireEnebled:
 			# Instantiate a bullet
 			var _bullet = bullet.instantiate()
-			_bullet.position = global_position
+			# Positioning the bullet. Correcting the bullet position adding an offset from pivot to position it at the exit hole of the weapon , the right way should be changing the pivot position
+			_bullet.position = global_position + Vector3(-owner.get_armature().get_global_transform().basis.z.x / 2, 0.1, -owner.get_armature().get_global_transform().basis.z.z / 2)
+			# Establishing the bullet direction, the speed is configured in the component
 			_bullet.get_movementComponent().direction =  -owner.get_armature().get_global_transform().basis.z
+			# Putting the bullet in the game
 			get_tree().get_root().add_child(_bullet)
 			
 			# Disable the fire system until the button is released
